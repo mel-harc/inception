b78ef8177ea3a460881cc57c3776b0f8816b496f
ipman <you>
2024-05-22 17:52:49 +0100

    last modification

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
 all :
-	mkdir -p /home/melharc/data/wp
-	mkdir -p /home/melharc/data/db
 	docker-compose -f ./srcs/docker-compose.yml up
 
 down:
 	docker-compose -f ./srcs/docker-compose.yml down
+	docker volume rm DataBase WordPress
+	docker system prune -a
+	cd .. && sudo rm -rf data
 
 stop:
 	docker-compose -f ./srcs/docker-compose.yml stop
+
+restart:
+	docker-compose -f ./srcs/docker-compose.yml restart
 
 re : down all
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -2,8 +2,6 @@
 
 sleep 6
 
-
-
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
@@ -20,7 +18,7 @@ wp config set DB_USER $MARIA_DB_USER --allow-root --path=/var/www/html
 wp config set DB_PASSWORD $MARIA_DB_USER_PASSWORD --allow-root --path=/var/www/html
 wp config set DB_HOST 'mariadb:3306' --allow-root --path=/var/www/html
 
-wp core install --url=$DOMAIN_NAME --title=INCEPTION --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root --path=/var/www/html
+wp core install --url=$DOMAIN_NAME --title=OwrApplication --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root --path=/var/www/html
 
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root --path=/var/www/html
 
